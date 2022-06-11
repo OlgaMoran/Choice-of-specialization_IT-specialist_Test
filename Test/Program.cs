96bed56685a1d43226e2e4a5a3fb0db8eb0d6e93
Olga Moran <olga@azimuth.co.za>
2022-06-11 21:43:53 +0200

    Added the task and the examples

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+// сформирует массив из строк, длина которых меньше или равна 3 символам. 
+// Первоначальный массив можно задать с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Examples:
+// ["hello", "2", "world", ":-)"]  => ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"]  => ["-2"]
+// ["Russia", "Denmark", "-Kazan"]  => []
+
+// * * * * * * * * * * * * * * * * * *
+// For testing - please uncomment:
+// string[] array1 = new string[] { "hello", "2", "world", ":-)" };
+// string[] array2 = new string[] { "1234", "1567", "-2", "computer science" };
+// string[] array3 = new string[] { "Russia", "Denmark", "Kazan" };
+
+
